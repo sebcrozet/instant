4f58ea88f8f07ca319479679f0df13e01407ccf6
Andrew Mackenzie <andrew@mackenzie-serres.net>
2019-06-15 13:27:31 -0400

    Add wasm_bindgen_test tests for Instant and Duration

---
+++ tests/wasm.rs
@@ -0,0 +1,21 @@
+extern crate wasm_bindgen_test;
+
+use wasm_bindgen_test::*;
+use instant::Instant;
+use std::time::Duration;
+
+wasm_bindgen_test_configure!(run_in_browser);
+// run these tests using: wasm-pack test --chrome --headless -- --features wasm-bindgen
+
+#[wasm_bindgen_test]
+fn test_instant_now() {
+    let now = Instant::now();
+    assert!(now.elapsed().as_nanos() > 0);
+}
+
+#[wasm_bindgen_test]
+fn test_duration() {
+    let now = Instant::now();
+    let one_sec = Duration::from_secs(1);
+    assert!(now.elapsed() < one_sec);
+}
