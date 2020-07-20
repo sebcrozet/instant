5c5f6519dbae89a30e1a52a735c2b7e74c7a0811
Noah <33094578+coolreader18@users.noreply.github.com>
2020-07-20 13:30:44 -0500

    Fix for wasm32-wasi target

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,48 @@
 #[cfg(all(
-    any(target_arch = "wasm32", target_arch = "asmjs"),
+    any(
+        all(target_arch = "wasm32", not(target_os = "wasi")),
+        target_arch = "asmjs"
+    ),
     all(feature = "stdweb", not(feature = "wasm-bindgen"))
 ))]
 #[macro_use]
 extern crate stdweb;
 
-#[cfg(not(any(target_arch = "wasm32", target_arch = "asmjs")))]
+#[cfg(not(any(
+    all(target_arch = "wasm32", not(target_os = "wasi")),
+    target_arch = "asmjs"
+)))]
 pub use crate::native::Instant;
 
-#[cfg(any(target_arch = "wasm32", target_arch = "asmjs"))]
+#[cfg(any(
+    all(target_arch = "wasm32", not(target_os = "wasi")),
+    target_arch = "asmjs"
+))]
 pub use crate::wasm::Instant;
 
-#[cfg(not(any(target_arch = "wasm32", target_arch = "asmjs")))]
+#[cfg(not(any(
+    all(target_arch = "wasm32", not(target_os = "wasi")),
+    target_arch = "asmjs"
+)))]
 #[cfg(feature = "now")]
 pub use crate::native::now;
 
-#[cfg(any(target_arch = "wasm32", target_arch = "asmjs"))]
+#[cfg(any(
+    all(target_arch = "wasm32", not(target_os = "wasi")),
+    target_arch = "asmjs"
+))]
 #[cfg(feature = "now")]
 pub use crate::wasm::now;
 
 pub use std::time::Duration;
 
-#[cfg(not(any(target_arch = "wasm32", target_arch = "asmjs")))]
+#[cfg(not(any(
+    all(target_arch = "wasm32", not(target_os = "wasi")),
+    target_arch = "asmjs"
+)))]
 mod native;
-#[cfg(any(target_arch = "wasm32", target_arch = "asmjs"))]
+#[cfg(any(
+    all(target_arch = "wasm32", not(target_os = "wasi")),
+    target_arch = "asmjs"
+))]
 mod wasm;
